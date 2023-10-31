9fa92442d6994208ee414525cb74469fc714f456
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-30 23:08:55 -0500

    reworked switch statment

--- index.js
+++ index.js
@@ -1,68 +1,88 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const SVG = require('svg.js');
-const {Circle, Square, Triangle} = require('./lib/shape');
-
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const SVG = require("svg.js");
+const { Circle, Square, Triangle } = require("./lib/shape");
 
 const questions = [
-    {
-        type: 'input',
-        message: 'What 3 letters do you want to use?',
-        name: 'initials',
-        validate:(value)=>{if(value){return true} else {return 'Input required'}}
+  {
+    type: "input",
+    message: "What 3 letters do you want to use?",
+    name: "initials",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Input required";
+      }
     },
-    {
-        type: 'input',
-        message: 'What color do you want the text? (enter any color or hexdecimal code)',
-        name: 'initialColor',
-        validate:(value)=>{if(value){return true} else {return 'Input required'}}
+  },
+  {
+    type: "input",
+    message:
+      "What color do you want the text? (enter any color or hexdecimal code)",
+    name: "initialColor",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Input required";
+      }
     },
-    {
-        type: 'list',
-        message: 'What shape would you  like?',
-        name: 'shape',
-        choices: ['circle', 'square', 'triangle'],
-        validate:(value)=>{if(value){return true} else {return 'Input required'}}
+  },
+  {
+    type: "list",
+    message: "What shape would you  like?",
+    name: "shape",
+    choices: ["Circle", "Square", "Triangle"],
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Input required";
+      }
     },
-    {
-        type: 'input',
-        message: 'What color do you want your shape? (enter any color or hexdecimal code)',
-        name: 'shapeColor',
-        validate:(value)=>{if(value){return true} else {return 'Input required'}}
+  },
+  {
+    type: "input",
+    message:
+      "What color do you want your shape? (enter any color or hexdecimal code)",
+    name: "shapeColor",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "Input required";
+      }
     },
+  },
 ];
 
-
 function writeToFile(fileName, data) {
-   console.log(typeof(data)); 
+  console.log(typeof data);
 
-    fs.writeFile(fileName, data, err => {
-        console.log(fileName)
-        console.log(data)
-       err? console.log(err) : console.log('sucess!')
-    });
+  fs.writeFile(fileName, data, (err) => {
+    console.log(fileName);
+    console.log(data);
+    err ? console.log(err) : console.log("sucess!");
+  });
 }
 
 function init() {
-    inquirer.prompt(questions)
-    .then(function(data) {
-        switch (`$(data.shape)`) {
-            case 'Circle':
-                const circle = new Circle(data.initals, data.initialColor, data.shape, data.shapeColor)
-                writeToFile("examples/logo.svg", Circle(data));
-                break;
-            case 'Square':
-                const square = new Square(data.initals, data.initialColor, data.shape, data.shapeColor)
-                writeToFile("examples/logo.svg", Square(data));
-                break;
-            case 'Triangle':
-                const triangle = new Triangle(data.initals, data.initialColor, data.shape, data.shapeColor)
-                writeToFile("examples/logo.svg", Triangle(data));
-                break;
-        }
-
-    });
+  inquirer.prompt(questions).then((data) => {
+    let shape;
+    switch (data.shape) {
+      case "Circle":
+        shape = new Circle(data.initials, data.initialColor, data.shapeColor);
+        break;
+      case "Square":
+        shape = new Square(data.initials, data.initialColor, data.shapeColor);
+        break;
+      default:
+        shape = new Triangle(data.initials, data.initialColor, data.shapeColor);
+        break;
+    }
+    writeToFile("examples/logo.svg", shape.render());
+  });
 }
 
 init();
